41e01e8265ec15c587217026de4af04afe75f37a
elad770 <73057751+elad770@users.noreply.github.com>
2022-11-04 16:06:23 +0200

    update url to new server

--- backend/settings.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values, load_dotenv
-from datetime import timedelta
-import os
-load_dotenv()
-
-# config = os.getenv("MONGO_URI") #dotenv_values(".env")
-MONGO_URI = os.getenv("MONGO_URI")  # config['MONGO_URI']
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")  # config['JWT_SECRET_KEY']
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=10)
